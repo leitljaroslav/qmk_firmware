22b3f1e61769c18288967e95fe23f2ed42f4c26d
Jaroslav Leitl <leitl.jaroslav@gmail.com>
2024-04-20 09:02:37 +0200

    WIP diy redox handwired

--- keyboards/redox/keymaps/default/config.h
+++ keyboards/redox/keymaps/default/config.h
@@ -20,7 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Select hand configuration */
 #define MASTER_LEFT
 // #define MASTER_RIGHT
-// #define EE_HANDS
+#define EE_HANDS
+#define USE_SERIAL
 
 #undef RGBLIGHT_LED_COUNT
 #define RGBLIGHT_EFFECT_BREATHING
@@ -37,3 +38,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+
